d4e70c8702ce47277b68af9439b5c950a4b7b6fb
Yesid Hernandez <yexid.9901@gmail.com>
2022-11-08 08:05:36 -0500

    Skeleton project

---
+++ src/main.ts
@@ -0,0 +1,30 @@
+import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { AppModule } from './app.module';
+
+(async function() {
+  const port = process.env.PORT || 3000;
+  const app = await NestFactory.create(AppModule, {
+    cors: true,
+  });
+
+  app.useGlobalPipes(new ValidationPipe({
+    transform: true,
+    whitelist: true,
+    forbidNonWhitelisted: true,
+  }))
+
+  const swaggerConfig = new DocumentBuilder()
+  .setTitle('Blue Project')
+  .setDescription('Basic project')
+  .setVersion('1.0')
+  .build();
+  const swaggerDocument = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api', app, swaggerDocument);
+
+  app.setGlobalPrefix('api');
+  await app.listen(port, async () => {
+    console.log(`Runing app in ${ await app.getUrl() }`);
+  });
+})()
